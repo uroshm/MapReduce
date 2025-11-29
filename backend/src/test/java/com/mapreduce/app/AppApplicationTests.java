a601f4c91a4f3b85d6c0cdbbcf183a6b38d81940
Uros <Uros@Enlightenary.com>
2025-11-29 12:21:26 -0500

    add easy medium hard scenarios to tests

--- backend/src/test/java/com/mapreduce/app/AppApplicationTests.java
+++ backend/src/test/java/com/mapreduce/app/AppApplicationTests.java
@@ -20,9 +20,9 @@ class AppApplicationTests {
 	private Orchestrator orchestrator;
 
 	@Test
-	void partitionNaiveHash() throws Exception {
-		var tweets = generateTweets();
-		orchestrator.initializeMappers(tweets, 4);
+	void partitionNaiveHashHardScenario() throws Exception {
+		var tweetsHardScenario = getHashtagData(hashtagsHardScenario);
+		orchestrator.initializeMappers(tweetsHardScenario, 4);
 		orchestrator.initializeReducers(2);
 		orchestrator.runMapReduce(PartitionStrategy.NAIVE);
 		Thread.sleep(8000);
@@ -30,8 +30,20 @@ class AppApplicationTests {
 	}
 
 	@Test
+	void partitionNaiveHashEasyScenario() throws Exception {
+		var tweetsHardScenario = getHashtagData(hashtagsEasyScenario);
+		orchestrator.initializeMappers(tweetsHardScenario, 4);
+		orchestrator.initializeReducers(2);
+		orchestrator.runMapReduce(PartitionStrategy.NAIVE);
+		Thread.sleep(8000);
+		System.out.println(orchestrator.collectResults());
+	}
+
+
+
+	@Test
 	void partitionSmart() throws Exception {
-		var tweets = generateTweets();
+		var tweets = getHashtagData(hashtagsHardScenario);
 		orchestrator.initializeMappers(tweets, 4);
 		orchestrator.initializeReducers(2);
 		orchestrator.runMapReduce(PartitionStrategy.EQUALLY_WEIGHTED);
@@ -39,15 +51,31 @@ class AppApplicationTests {
 		System.out.println(orchestrator.collectResults());
 	}
 
-	private List<Tweet> generateTweets() {
-		Map<String, Integer> hashtags = Map.of(
-				"#Basketball", 200,
-				"#Soccer", 4000,
-				"#Football", 100,
-				"#Racquetball", 200,
-				"#Pickleball", 300,
-				"#Boxing", 100);
+	Map<String, Integer> hashtagsHardScenario = Map.of(
+			"#Basketball", 200,
+			"#Soccer", 4000,
+			"#Football", 100,
+			"#Racquetball", 200,
+			"#Pickleball", 300,
+			"#Boxing", 100);
 
+	Map<String, Integer> hashtagsMediumScenario = Map.of(
+			"#Basketball", 200,
+			"#Soccer", 4000,
+			"#Football", 100,
+			"#Racquetball", 200,
+			"#Pickleball", 300,
+			"#Boxing", 100);
+
+	Map<String, Integer> hashtagsEasyScenario = Map.of(
+			"#Basketball", 200,
+			"#Soccer", 200,
+			"#Football", 200,
+			"#Racquetball", 200,
+			"#Pickleball", 200,
+			"#Boxing", 200);			
+
+	private List<Tweet> getHashtagData(Map<String, Integer> hashtags) {
 		ArrayList<Tweet> tweets = new ArrayList<>();
 		for (var entry : hashtags.entrySet()) {
 			int count = entry.getValue();
